b0cb1ebdced2a5cfff3ee34c953bd8bdbff2ebd0
stepanchaparyan <stepanchaparyan@gmail.com>
2020-03-17 01:44:18 +0400

    Added mongoDB connection

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+const connectDB = require('./config/mongoDB');
+//connet to mongoDB
+connectDB();
+
+app.use(express.json({ extended: true }));
 
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`server started at port ${PORT}`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server started at port ${PORT}`));
