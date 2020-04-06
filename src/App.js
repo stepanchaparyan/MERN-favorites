469fc1b3bbfec3b405adf23624f3737abe5d3f37
stepanchaparyan <stepanchaparyan@gmail.com>
2020-04-07 00:17:33 +0400

    Rename server.js to app.js
    - added * route

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ const App = () => {
               <PrivateRoute exact path="/" component={Home} />
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="*" component={Login} />
             </Switch>
             <Footer />
           </div>
